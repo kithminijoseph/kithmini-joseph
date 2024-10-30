ef9031688fa4b3bbbd7f2cac81fb22a7dfd5a29f
Kithmini Joseph <KithminiJoseph@ibm.com>
2024-10-30 16:02:59 +0000

    update background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import ParticlesBg from 'particles-bg'
+import ParticlesBg from 'particles-bg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -8,10 +8,10 @@ import Projects from './pages/Projects';
 import Education from './pages/Education';
 import './App.css';
 
-
 function App() {
   return (
     <Router>
+      <ParticlesBg color="#ff0000, #ff1111 " num={20} type="circle" bg={true} />
       <Navbar />
       <div className="content">
         <Routes>
@@ -25,16 +25,3 @@ function App() {
 }
 
 export default App;
-
-<ParticlesBg color="#ff0000" num={200} type="circle" bg={true} />
-
-class Example extends Component {
-  render () {
-    return (
-      <>
-        <div>...</div>
-        <ParticlesBg type="circle" bg={true} />
-      </>
-    )
-  }
-}

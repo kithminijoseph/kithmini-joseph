ee61feaf878e00376edea44829601108263a6a9b
Kithmini Joseph <KithminiJoseph@ibm.com>
2024-10-30 15:56:58 +0000

    update package

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // src/App.jsx
 import React from 'react';
+import ParticlesBg from 'particles-bg'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Education from './pages/Education';
 import './App.css';
+
 
 function App() {
   return (
@@ -23,3 +25,16 @@ function App() {
 }
 
 export default App;
+
+<ParticlesBg color="#ff0000" num={200} type="circle" bg={true} />
+
+class Example extends Component {
+  render () {
+    return (
+      <>
+        <div>...</div>
+        <ParticlesBg type="circle" bg={true} />
+      </>
+    )
+  }
+}

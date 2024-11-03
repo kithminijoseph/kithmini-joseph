574d109319bab582312761545fc36eca121a0316
Kithmini Joseph <KithminiJoseph@ibm.com>
2024-11-03 01:31:22 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import ParticlesBg from 'particles-bg';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
@@ -18,19 +17,17 @@ function App() {
 
   return (
     <Router>
-      <Routes>
       <div className="background-layer" />
-        <ParticlesBg color={["#ff0000", "#ff6347", "#ff7f50", "#ffa07a", "#ffd700"]} num={10} type="circle" bg={true} />
-        <Navbar setTheme={setTheme} theme={theme} />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/education" element={<Education />} />
-          </Routes>
+      <ParticlesBg color={["#ff0000", "#ff6347", "#ff7f50", "#ffa07a", "#ffd700"]} num={10} type="circle" bg={true} />
+      <Navbar setTheme={setTheme} theme={theme} />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/education" element={<Education />} />
+        </Routes>
       </div>
-      </Routes>
     </Router>
   );
 }

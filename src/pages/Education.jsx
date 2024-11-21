e1b1feb2b135cefbd26117e9c4b950f71f6874b5
Kithmini Joseph <KithminiJoseph@ibm.com>
2024-11-21 16:23:52 +0000

    fixing scrolling

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import './Education.css';
+import university from '../assets/university.svg';
 
 function Education() {
     return (
         <div>
-            <h2>Education</h2>
-            <p>Here’s my educational background!</p>
+             <div className="intro">
+                <h>University of Bath</h>
+                <p>Computer Science & Artificial Intelligence BSc (2022 - 2026) </p>
+                <p1> First Year: {"\n"} 64 %  </p1>
+                <p2> Second Year: {"\n"} 67 % </p2>
+                <p3> Third Year: {"\n"} Software Engineering Year Long Placement at IBM UK </p3>
+                <p4> Fourth Year: {"\n"} Software Engineering Year Long Placement at IBM UK </p4>
+                <img src={university} alt="Profile" className="pic" />
+             </div>
         </div>
     );
 }

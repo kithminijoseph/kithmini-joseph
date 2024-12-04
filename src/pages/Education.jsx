aa63597b7b8bdddbb01d8eaee51db173ad7a16c1
Kithmini Joseph <KithminiJoseph@ibm.com>
2024-12-04 23:09:08 +0000

    update education

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Education.css';
-import university from '../assets/university.svg';
+import university from '../assets/university1.svg';
 
 function Education() {
     return (
@@ -8,10 +8,11 @@ function Education() {
              <div className="intro">
                 <h>University of Bath</h>
                 <p>Computer Science & Artificial Intelligence BSc (2022 - 2026) </p>
-                <p1> First Year: {"\n"} 64 %  </p1>
-                <p2> Second Year: {"\n"} 67 % </p2>
-                <p3> Third Year: {"\n"} Software Engineering Year Long Placement at IBM UK </p3>
-                <p4> Fourth Year: {"\n"} Software Engineering Year Long Placement at IBM UK </p4>
+                <p1> First Year: 64 %    </p1>
+                <p2> Second Year: 67 % </p2>
+                <p3> Third Year: Software Engineering Year Long Placement at IBM UK </p3>
+                <p4> Fourth Year:  - </p4>
+                <p5> Notable Achievements: - </p5>
                 <img src={university} alt="Profile" className="pic" />
              </div>
         </div>
